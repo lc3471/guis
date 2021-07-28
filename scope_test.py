b512bfbc1c43b599a6430340caf10722e56c3c70
lc3471 <86745596+lc3471@users.noreply.github.com>
2021-07-28 10:01:35 -0400

    Add files via upload

--- scope_test.py
+++ scope_test.py
@@ -2,22 +2,24 @@ import visa
 import pylab as pl
 import datetime
 import numpy as np
+import pandas as pd
+
 
 rm=visa.ResourceManager()
 inst=rm.open_resource('USB0::62701::60986::SDS1ECDD2R8216::0::INSTR')
 inst.write("CHDR OFF")
 
+vdiv=inst.query("C1:VDIV?")
+ofst=inst.query("C1:OFST?")
+tdiv=inst.query("TDIV?")
+sara=inst.query("SARA?")
+
+inst.write("WFSU FP,0,NP,1000,SP,1")
+
+inst.timeout=30000
+inst.chunk_size=20*1024**2
 
 def wf():
-    vdiv=inst.query("C1:VDIV?")
-    ofst=inst.query("C1:OFST?")
-    tdiv=inst.query("TDIV?")
-    sara=inst.query("SARA?")
-
-    inst.write("WFSU FP,0,NP,1000,SP,1")
-
-    inst.timeout=30000
-    inst.chunk_size=20*1024**2
     inst.write("C1:WF? DAT2")
     ret=list(inst.read_raw())[16:]
     ret.pop()
@@ -50,4 +52,22 @@ def data_to_array():
     array=np.array([t,v]).transpose()
     return array
 
-print(data_to_array())
+#f=open('/media/ctalab/DATA/CTA/testing/waveform/07_27_2021/test500.txt','a')
+
+t,v=wf()
+df=pd.DataFrame(data=[v],columns=t)
+
+
+
+for n in range(500):
+    t,v=wf()
+    df2=pd.DataFrame(data=[v],columns=t)
+    df=df.append(df2,ignore_index=True)
+
+df=df.transpose()
+
+wdir = "/home/ctalab/data/CTA/testing/waveform/07_27_2021/"
+df.to_csv(wdir+"35_5.csv")
+
+#f.close()
+
